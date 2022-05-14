aaf91765fc0f98d3a1d8ff40311b5cba26dcfeb3
vijaylpersad <vijaylpersad@utexas.edu>
2022-05-14 18:38:57 -0500

    working mario game concept

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -86,6 +86,32 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/background.png":
+/*!****************************!*\
+  !*** ./src/background.png ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "072d51bcc9c09311d4c2a6708b05bddc.png");
+
+/***/ }),
+
+/***/ "./src/hills.png":
+/*!***********************!*\
+  !*** ./src/hills.png ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "cfffe4c371f5e11d372b398a87c51dd0.png");
+
+/***/ }),
+
 /***/ "./src/js/canvas.js":
 /*!**************************!*\
   !*** ./src/js/canvas.js ***!
@@ -95,8 +121,9 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _mario_platform_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../mario_platform.png */ "./src/mario_platform.png");
+/* harmony import */ var _background_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../background.png */ "./src/background.png");
+/* harmony import */ var _hills_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../hills.png */ "./src/hills.png");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -104,105 +131,322 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 
+
+ //console.log(platform)
+
 var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var c = canvas.getContext("2d");
+canvas.width = 1024;
+canvas.height = 576; //16:9 aspect ratio
 
-addEventListener('mousemove', function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
-}); // Objects
+var gravity = 0.5; //to create any sort of player or art on canvas create a class is a good start
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
+var Player = /*#__PURE__*/function () {
+  function Player() {
+    _classCallCheck(this, Player);
 
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+    this.height = 30;
+    this.width = 30;
+  } //method to define how player looks:
 
-  _createClass(Object, [{
+
+  _createClass(Player, [{
     key: "draw",
     value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
-    }
+      c.fillStyle = 'red';
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    } //need to create an update function - method to change player property over time
+
   }, {
     key: "update",
     value: function update() {
       this.draw();
+      this.position.y += this.velocity.y;
+      this.position.x += this.velocity.x; //establishing a floor and factoring gravity into fall
+
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+        //if player is higher than bottom of screen, keep building velocity, else velocity = 0
+        this.velocity.y += gravity;
+      } //else this.velocity.y = 0
+
     }
   }]);
 
-  return Object;
-}(); // Implementation
+  return Player;
+}(); //build platforms for player to jump onto and avoid pit of death
 
 
-var objects;
+var Platform = /*#__PURE__*/function () {
+  function Platform(_ref) {
+    var x = _ref.x,
+        y = _ref.y,
+        image = _ref.image;
 
-function init() {
-  objects = [];
+    _classCallCheck(this, Platform);
 
-  for (var i = 0; i < 400; i++) {// objects.push()
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
-} // Animation Loop
+
+  _createClass(Platform, [{
+    key: "draw",
+    value: function draw() {
+      //  c.fillStyle = 'blue'
+      //  c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return Platform;
+}(); //background stuff
+
+
+var genericObject = /*#__PURE__*/function () {
+  function genericObject(_ref2) {
+    var x = _ref2.x,
+        y = _ref2.y,
+        image = _ref2.image;
+
+    _classCallCheck(this, genericObject);
+
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+
+  _createClass(genericObject, [{
+    key: "draw",
+    value: function draw() {
+      //  c.fillStyle = 'blue'
+      //  c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return genericObject;
+}(); //create a function to take argument of image source so we don't have to keep calling on specific srcs around. DRY
+
+
+function createImage(imageSrc) {
+  var image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+
+var platformImage = createImage(_mario_platform_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
+var genericObjects = [new genericObject({
+  x: -1,
+  //where we want it to start
+  y: -1,
+  image: createImage(_background_png__WEBPACK_IMPORTED_MODULE_1__["default"])
+}), new genericObject({
+  x: -1,
+  y: -1,
+  image: createImage(_hills_png__WEBPACK_IMPORTED_MODULE_2__["default"])
+})];
+var player = new Player(); //const platform = new Platform()
+
+var platforms = [new Platform({
+  x: -1,
+  y: 470,
+  image: platformImage
+}), new Platform({
+  x: platformImage.width + 200,
+  y: 200,
+  image: platformImage
+}), new Platform({
+  x: platformImage.width * 2 + 400,
+  y: 470,
+  image: platformImage
+})];
+var keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+}; //player.update()
+
+var scrollOffset = 0; //when player dies we are going to call function restart to reset player back to start position
+
+function restart() {
+  platformImage = createImage(_mario_platform_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
+  genericObjects = [new genericObject({
+    x: -1,
+    //where we want it to start
+    y: -1,
+    image: createImage(_background_png__WEBPACK_IMPORTED_MODULE_1__["default"])
+  }), new genericObject({
+    x: -1,
+    y: -1,
+    image: createImage(_hills_png__WEBPACK_IMPORTED_MODULE_2__["default"])
+  })];
+  player = new Player(); //const platform = new Platform()
+
+  platforms = [new Platform({
+    x: -1,
+    y: 470,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width + 200,
+    y: 200,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width * 2 + 400,
+    y: 470,
+    image: platformImage
+  })]; //player.update()
+
+  scrollOffset = 0;
+} //need animation funciton to loop player update
 
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  c.fillStyle = 'white';
+  c.fillRect(0, 0, canvas.width, canvas.height); //white background for playable area
+  //platform.draw()
+  //draw background first:
+
+  genericObjects.forEach(function (genericObject) {
+    genericObject.draw();
+  });
+  platforms.forEach(function (platform) {
+    platform.draw();
+  });
+
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0; //set player velocity to zero and start moving platform backwards to create illusion of movement
+
+    if (keys.right.pressed) {
+      platforms.forEach(function (platform) {
+        platform.position.x -= 5;
+        scrollOffset += 5;
+      });
+      genericObjects.forEach(function (genericObject) {
+        genericObject.position.x -= 3;
+      });
+    } else if (keys.left.pressed) {
+      platforms.forEach(function (platform) {
+        platform.position.x += 5;
+        scrollOffset -= 5;
+      });
+      genericObjects.forEach(function (genericObject) {
+        genericObject.position.x += 3;
+      });
+    }
+  } //if player is on top of platform, don't fall anymore -- colloision detection
+
+
+  platforms.forEach(function (platform) {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0;
+    }
+  }); //win condition
+
+  if (scrollOffset > 9400) {
+    console.log('winner');
+  } //lose condition
+
+
+  if (player.position.y > canvas.height) {
+    console.log('you lose');
+    restart();
+  }
+
+  player.update();
 }
 
-init();
 animate();
+addEventListener('keydown', function (_ref3) {
+  var keyCode = _ref3.keyCode;
+  //object destructuring of keyboard event to retrieve keyCode
+  console.log(keyCode);
+
+  switch (keyCode) {
+    case 65:
+      console.log('left');
+      keys.left.pressed = true;
+      break;
+
+    case 83:
+      console.log('down');
+      break;
+
+    case 68:
+      console.log('right'); //player.velocity.x = 5
+
+      keys.right.pressed = true;
+      break;
+
+    case 87:
+      console.log('up');
+      player.velocity.y -= 20;
+      break;
+  }
+});
+addEventListener('keyup', function (_ref4) {
+  var keyCode = _ref4.keyCode;
+  //object destructuring of keyboard event to retrieve keyCode
+  console.log(keyCode);
+
+  switch (keyCode) {
+    case 65:
+      console.log('left');
+      keys.left.pressed = false;
+      break;
+
+    case 83:
+      console.log('down');
+      break;
+
+    case 68:
+      console.log('right'); //player.velocity.x = 0
+
+      keys.right.pressed = false;
+      break;
+
+    case 87:
+      console.log('up');
+      player.velocity.y -= 0;
+      break;
+  }
+});
 
 /***/ }),
 
-/***/ "./src/js/utils.js":
-/*!*************************!*\
-  !*** ./src/js/utils.js ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/***/ "./src/mario_platform.png":
+/*!********************************!*\
+  !*** ./src/mario_platform.png ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
-
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "abfd1ce5f662f8cde0ddcdf35cf855f8.png");
 
 /***/ })
 
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,287 @@
-import utils from './utils'
-
+import platform from '../mario_platform.png'
+import background from '../background.png'
+import hills from '../hills.png'
+//console.log(platform)
 const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+const c = canvas.getContext("2d")
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+canvas.width = 1024
+canvas.height = 576 //16:9 aspect ratio
+
+const gravity = 0.5
+//to create any sort of player or art on canvas create a class is a good start
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.height = 30
+        this.width = 30
+    }
+    //method to define how player looks:
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    }
+
+    //need to create an update function - method to change player property over time
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
+
+        //establishing a floor and factoring gravity into fall
+        if (this.position.y + this.height + this.velocity.y <= canvas.height){ //if player is higher than bottom of screen, keep building velocity, else velocity = 0
+            this.velocity.y += gravity
+        } //else this.velocity.y = 0
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+//build platforms for player to jump onto and avoid pit of death
+class Platform {
+   constructor({ x, y, image }){
+       this.position = {
+           x:x,
+           y:y
+       }
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
+       this.image = image
+       this.width = image.width
+       this.height = image.height
+       
+   }
+   
+   draw() {
+      //  c.fillStyle = 'blue'
+      //  c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      c.drawImage(this.image, this.position.x, this.position.y)
+   }
+}
 
-  init()
-})
+//background stuff
+class genericObject {
+  constructor({ x, y, image }){
+      this.position = {
+          x:x,
+          y:y
+      }
 
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+      this.image = image
+      this.width = image.width
+      this.height = image.height
+      
   }
-
+  
   draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
+     //  c.fillStyle = 'blue'
+     //  c.fillRect(this.position.x, this.position.y, this.width, this.height)
+     c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+//create a function to take argument of image source so we don't have to keep calling on specific srcs around. DRY
+
+function createImage(imageSrc){
+  const image = new Image()
+  image.src = imageSrc
+  return image
 }
 
-// Animation Loop
+let platformImage = createImage(platform)
+
+
+let genericObjects = [
+  new genericObject({
+    x: -1, //where we want it to start
+    y: -1,
+    image: createImage(background)
+  }),
+  new genericObject({
+    x: -1,
+    y: -1,
+    image: createImage(hills)
+  })
+]
+
+let player = new Player()
+//const platform = new Platform()
+let platforms = [
+  new Platform({
+    x: -1, 
+    y: 470, 
+    image:platformImage}), 
+  new Platform({
+    x: platformImage.width+200, 
+    y: 200, 
+    image:platformImage}),
+  new Platform({
+    x: platformImage.width*2 + 400, 
+    y: 470, 
+    image:platformImage})
+]
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+//player.update()
+let scrollOffset = 0
+
+//when player dies we are going to call function restart to reset player back to start position
+function restart(){
+
+  platformImage = createImage(platform)
+
+  genericObjects = [
+    new genericObject({
+      x: -1, //where we want it to start
+      y: -1,
+      image: createImage(background)
+    }),
+    new genericObject({
+      x: -1,
+      y: -1,
+      image: createImage(hills)
+    })
+  ]
+
+  player = new Player()
+  //const platform = new Platform()
+  platforms = [
+    new Platform({
+      x: -1, 
+      y: 470, 
+      image:platformImage}), 
+    new Platform({
+      x: platformImage.width+200, 
+      y: 200, 
+      image:platformImage}),
+    new Platform({
+      x: platformImage.width*2 + 400, 
+      y: 470, 
+      image:platformImage})
+]
+
+  //player.update()
+  scrollOffset = 0
+}
+
+//need animation funciton to loop player update
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(animate)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height) //white background for playable area
+    //platform.draw()
+    //draw background first:
+    genericObjects.forEach(genericObject => {
+      genericObject.draw()
+    })
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+
+    if (keys.right.pressed && player.position.x < 400){
+        player.velocity.x = 5
+    } else if (keys.left.pressed && player.position.x > 100){
+        player.velocity.x = -5
+    } else {
+        player.velocity.x = 0
+        //set player velocity to zero and start moving platform backwards to create illusion of movement
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+                scrollOffset +=5
+            })
+            genericObjects.forEach(genericObject => {
+              genericObject.position.x -=3
+            })
+        } else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x += 5
+                scrollOffset -= 5
+            })
+            genericObjects.forEach(genericObject => {
+              genericObject.position.x +=3
+            })
+        }
+    }
+
+    //if player is on top of platform, don't fall anymore -- colloision detection
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
+            player.velocity.y = 0
+        }
+    })
+
+    //win condition
+    if (scrollOffset > 9400){
+        console.log('winner')
+    }
+
+    //lose condition
+    if (player.position.y > canvas.height){
+      console.log('you lose')
+      restart()
+    }
+    player.update()
 }
 
-init()
 animate()
+
+addEventListener('keydown', ({ keyCode })=> { //object destructuring of keyboard event to retrieve keyCode
+    console.log(keyCode)
+    switch(keyCode){
+            case 65:
+                console.log('left')
+                keys.left.pressed = true
+                break
+            case 83:
+                console.log('down')
+                break
+            case 68:
+                console.log('right')
+                //player.velocity.x = 5
+                keys.right.pressed = true
+                break
+            case 87:
+                console.log('up')
+                player.velocity.y -= 20
+                break
+    }
+})
+
+addEventListener('keyup', ({ keyCode })=> { //object destructuring of keyboard event to retrieve keyCode
+    console.log(keyCode)
+    switch(keyCode){
+            case 65:
+                console.log('left')
+                keys.left.pressed = false
+                break
+            case 83:
+                console.log('down')
+                break
+            case 68:
+                console.log('right')
+                //player.velocity.x = 0
+                keys.right.pressed = false
+                break
+            case 87:
+                console.log('up')
+                player.velocity.y -= 0
+                break
+    }
+})
